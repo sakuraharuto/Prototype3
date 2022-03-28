ed13025a905f5a2678f3c208aecf647daaee8636
YAKKAA <bangyuwang@qq.com>
2022-03-28 16:21:53 -0400

    Load Scene Function Completed

---
+++ Assets/ExitDarkRoom.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitDarkRoom : MonoBehaviour
+{
+    public int nextSceneToLoad;
+    void Start()
+    {
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        SceneManager.LoadScene(nextSceneToLoad);
+    }
+}
